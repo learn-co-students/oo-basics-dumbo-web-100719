c9fbcbdf1f8cab3ca12c0f26653c599e1c63fab5
Brianna Fetterley <bnfetterley@usfca.edu>
2019-10-08 19:50:18 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
-# Make your shoe class here!
+class Shoe
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  attr_accessor :brand, :color, :size, :material, :condition
+  # attr_reader :title
+ 
+  # def initialize(title)
+  #   @title = title
+  # end
+ 
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  
+  # @condition = new
+ 
+end# Make your shoe class here!
