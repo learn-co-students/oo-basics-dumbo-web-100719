3306a4593cfae86e46a37b8f7feee29d0498d7c7
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-10-08 17:53:23 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,11 @@
 # Make your shoe class here!
+class Shoe 
+  def initialize(brand)
+    @brand = brand
+  end
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+  attr_accessor :color, :size, :material, :condition, :brand
+end
