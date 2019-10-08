82c289538e2e21fc91c7608bd528e39dcfcd0069
RubenV-Dev <r.will16@hotmail.com>
2019-10-08 16:34:41 -0400

    changes 3

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class Shoe
+
+    def initialize
+        @brand = brand
+    end
+
+    def cobble
+        puts "The shoe has been repaired"
+        @condition = new
+    end
+end
