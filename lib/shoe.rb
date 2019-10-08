261c7d60f499f37332f10a0ac9d25dcb8a62b72d
RubenV-Dev <r.will16@hotmail.com>
2019-10-08 16:38:05 -0400

    a

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,9 @@
 # Make your shoe class here!
 class Shoe
+    attr_reader :brand, :color, :size, :material
+    attr_accessor :condition
 
-    def initialize
+    def initialize(brand)
         @brand = brand
     end
 
